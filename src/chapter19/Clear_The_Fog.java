597f6cbc149c9b499f55ceb7442357511526d108
ryesil <ramazanysl@gmail.com>
2021-08-11 11:03:33 -0500

    some more updates

--- src/chapter19/Clear_The_Fog.java
+++ src/chapter19/Clear_The_Fog.java
@@ -12,7 +12,7 @@ public class Clear_The_Fog {
          return "It's a clear day!";
      }
 
-
+       System.out.println("bu nedir ya ben neden ogrenemiyorum :(");
        System.out.println("Bu kapak olmadi");
 
        System.out.println("Bu da kapak olsun");
